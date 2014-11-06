0e7e9ee448968529a1275dc43475b46484dca564
Ahsan Rony <ahrony90@gmail.com>
2014-11-07 01:43:02 +0600

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,23 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def software
+  end
+
+  def web
+  end
+
+  def contact
+  end
+
+  def consultancy
+  end
+
+  def facebook
+  end
+
+end
